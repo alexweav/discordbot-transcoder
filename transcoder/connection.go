395afbe8c2dc8ed4a831ff236d0021cee40e91fe
Alex Weaver <weaver.alex.d@gmail.com>
2019-12-28 03:29:05 -0600

    Create channel

--- transcoder/connection.go
+++ transcoder/connection.go
@@ -8,6 +8,7 @@ import (
 
 type Connection struct {
 	Connection *amqp.Connection
+	Channel *amqp.Channel
 }
 
 func Connect(address string, port int, user string, pass string) (*Connection, error) {
@@ -17,11 +18,20 @@ func Connect(address string, port int, user string, pass string) (*Connection, e
 		log.Fatalf("Could not connect to RabbitMQ: %s", err)
 		return nil, err
 	}
+
+	channel, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("Could not open channel: %s", err)
+		return nil, err
+	}
+
 	return &Connection{
 		Connection: conn,
+		Channel: channel,
 	}, nil
 }
 
 func (connection *Connection) Close() {
+	connection.Channel.Close()
 	connection.Connection.Close()
 }
