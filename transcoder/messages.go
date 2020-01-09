be6a6bbb70ff558c53d739530ed701225fa44356
Alex Weaver <weaver.alex.d@gmail.com>
2020-01-08 23:27:22 -0600

    Comments

--- transcoder/messages.go
+++ transcoder/messages.go
@@ -2,15 +2,18 @@ package transcoder
 
 import "encoding/json"
 
+// An AMQP message.
 type Message interface {
 	Serialize() ([]byte, error)
 }
 
+// A command to perform an audio transcode job.
 type TranscodeCommand struct {
 	FileName string `json:"fileName"`
 	GuildId  string `json:"guildId"`
 }
 
+// Deserializes a TranscodeCommand from JSON.
 func ToTranscodeCommand(bytes []byte) (TranscodeCommand, error) {
 	var command TranscodeCommand
 	if err := json.Unmarshal(bytes, &command); err != nil {
@@ -19,6 +22,7 @@ func ToTranscodeCommand(bytes []byte) (TranscodeCommand, error) {
 	return command, nil
 }
 
+// Serializes a TranscodeCommand to JSON.
 func (command TranscodeCommand) Serialize() ([]byte, error) {
 	return json.Marshal(command)
 }
